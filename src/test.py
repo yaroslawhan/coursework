64e90ee258d6c08db755c47db34d9e8d3a2a3665
Han <yaroslav.khanevskiy@gmail.com>
2024-05-18 17:29:11 +0400

    Финальная версия

--- src/test.py
+++ src/test.py
@@ -1,36 +1,24 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("GeeksCoders.com")
+        self.setWindowTitle("Main Window")
+        self.setGeometry(100, 100, 600, 400)
+        # Создание всплывающего окна QMessageBox
+        msg_box = QMessageBox()
+        msg_box.setIcon(QMessageBox.Information)
+        msg_box.setWindowTitle("Message")
+        msg_box.setText("This is a message box.")
+        msg_box.setStandardButtons(QMessageBox.Ok)
 
-        # Create Open File button
-        open_button = QPushButton("Open File", self)
-        open_button.setGeometry(50, 50, 100, 30)
-        open_button.clicked.connect(self.open_file)
-
-        # Create Save File button
-        save_button = QPushButton("Save File", self)
-        save_button.setGeometry(50, 100, 100, 30)
-        save_button.clicked.connect(self.save_file)
-
-    def open_file(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Open File", ".", "Text Files (*.txt);;All Files (*)")
-        if filename:
-            with open(filename, 'r') as file:
-                contents = file.read()
-            print(contents)
-
-    def save_file(self):
-        filename, _ = QFileDialog.getSaveFileName(self, "Save File", ".", "Text Files (*.txt);;All Files (*)")
-        if filename:
-            with open(filename, 'w') as file:
-                file.write("Welcome to GeeksCoders.com")
+        # Показать всплывающее окно
+        msg_box.exec_()
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec_()
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec_())
